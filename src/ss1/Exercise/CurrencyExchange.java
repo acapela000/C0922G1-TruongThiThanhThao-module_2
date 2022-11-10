a593894160f89cd33999c58dd32dd946b630af8a
acapela000 <mrtrados@gmail.com>
2022-11-10 22:08:53 +0700

    FIX feedback from tutor

--- src/ss1/Exercise/CurrencyExchange.java
+++ src/ss1/Exercise/CurrencyExchange.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class CurrencyExchange {
     public static void main(String[] args) {
-        double vnd = 23000;
+        double vndToDollar = 23000;
         System.out.println("Input the number of usd u want to exchange");
         Scanner scanner = new Scanner(System.in);
         double usd = scanner.nextDouble();
-        System.out.println("The money u got is " + usd*vnd);
+        System.out.println("The money u got is " + usd*vndToDollar);
     }
 }
--- src/ss2/Exercise/PrimeNumberTill100.java
+++ src/ss2/Exercise/PrimeNumberTill100.java
@@ -4,19 +4,20 @@ import java.util.Scanner;
 
 public class PrimeNumberTill100 {
     public static void main(String[] args) {
-        System.out.println("Input a number");
-        Scanner sc = new Scanner(System.in);
-        int number = Integer.parseInt(sc.nextLine());
-        int array[] = new int[100];
-            for (int i = 2; i < 100; i++) {
-                boolean isPrime = true;
-                if (number % i == 0) {
-                    isPrime = false;
+
+//        int array[] = new int[100];
+
+        for (int i = 2; i < 100; i++) {
+            int count = 0;
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    count++;
                 }
-                if (isPrime) {
-                    System.out.println(array[i] + " is a prime number");
-                }
+            }
+            if (count == 2) {
+                System.out.println(i + " is a prime number");
             }
         }
     }
+}
 
