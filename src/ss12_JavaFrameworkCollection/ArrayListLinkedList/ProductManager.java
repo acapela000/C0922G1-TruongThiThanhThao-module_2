75d1dedd17f8708c41991108184b25d4cad5c348
acapela000 <mrtrados@gmail.com>
2022-11-22 22:10:01 +0700

    Editting @.@

--- src/ss12_JavaFrameworkCollection/ArrayListLinkedList/ProductManager.java
+++ src/ss12_JavaFrameworkCollection/ArrayListLinkedList/ProductManager.java
@@ -3,10 +3,69 @@ package ss12_JavaFrameworkCollection.ArrayListLinkedList;
 import java.util.*;
 
 public class ProductManager  {
+
     static List<Product> arrayList = new ArrayList<>();
-    static String editID;
-    static int editPrice;
-    static String editName;
+
+    public static void main(String[] args) {
+
+        Product product1 = new Product("A123", "SamSung", 15000);
+        Product product2 = new Product("B123", "iPhone", 20000);
+        Product product3 = new Product("C123", "BlackBerry", 5000);
+        Product product4 = new Product("D123", "SonyEricson", 4000);
+
+        arrayList.add(product1);
+        arrayList.add(product2);
+        arrayList.add(product3);
+        arrayList.add(product4);
+
+        sort(true);
+        System.out.println();
+        sort(false);
+        
+
+        System.out.println();
+        menu();
+    }
+
+    public static void menu() {
+        Scanner sc = new Scanner(System.in);
+
+        int choiceNumber;
+        do {
+            System.out.println("Choose from menu: [0.Exit] [1.Add] [2.Edit] [3.Delete] [4.Search] Product");
+            choiceNumber = Integer.parseInt(sc.nextLine());
+
+            if (choiceNumber >= 0 && choiceNumber < 5) {
+                if (choiceNumber == 0) {
+                    System.out.println("See U again!^^!");
+                    break;
+                }
+
+                if (choiceNumber == 1) { //Add
+                    addProduct();
+                    sort(true);
+                }
+
+                if (choiceNumber == 2) { //Edit
+                    editProduct();
+                    sort(true);
+                }
+
+                    if (choiceNumber == 3) { //Delete
+                        System.out.println("Input index of product U wanna delete, 0 =< index =< " + (arrayList.size()-1));
+                        int index = Integer.parseInt(sc.nextLine());
+
+                        arrayList.remove(index);
+                        sort(true);
+                    }
+
+                    if (choiceNumber == 4) { //Search
+                        searchProductMenu();
+                        System.out.println();
+                    }
+                }
+        } while (choiceNumber > 0 && choiceNumber < 5) ;
+    }
 
     private static int sortLogic(boolean isAscending, Product o1, Product o2) {
         if (isAscending) {
@@ -27,113 +86,66 @@ public class ProductManager  {
         }
     }
 
-    private static void addProduct(String addProductID,String addProductName,int addProductPrice) {
+    private static void addProduct() {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Input product ID: ");
-        addProductID = sc.nextLine();
+        String iD = sc.nextLine();
         System.out.print("Input product name: ");
-        addProductName = sc.nextLine();
+        String name = sc.nextLine();
         System.out.print("Input product Price: ");
-        addProductPrice = Integer.parseInt(sc.nextLine());
+        int price = Integer.parseInt(sc.nextLine());
 
         System.out.println("Your new added product: productID='"
-                + addProductID + "', productName='" + addProductName
-                + "', productPrice='" + addProductPrice + "'");
+                + iD + "', productName='" + name
+                + "', productPrice='" + price + "'");
 
-        Product product5 = new Product(addProductID, addProductName, addProductPrice);
+        Product product5 = new Product(iD, name, price);
         arrayList.add(product5);
     }
 
-    private static void editProduct(String editProductID,String editProductName,int editProductPrice) {
+    private static void editProduct() {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Input index of product U wanna edit: ");
-        int indexDeleteProct = Integer.parseInt(sc.nextLine());
+        System.out.print("Input the ID of the product U wanna edit: ");
+        String id = sc.nextLine();
+        Product product = searchProduct(id);
+        //Product here is a type of data & here the data type is object, instead of String or int  (***)
 
-        System.out.println("Product U wanna edit: " + arrayList.get(indexDeleteProct));
+        System.out.println("Product U wanna edit: " + product);
 
         System.out.print("Edit product ID: ");
-        String eidtProductID = arrayList.get(indexDeleteProct).setProductID(sc.nextLine());
+        product.setProductID(sc.nextLine());
 
         System.out.print("Edit product name: ");
-        String eidtProductName = arrayList.get(indexDeleteProct).setProductName(sc.nextLine());
+        product.setProductName(sc.nextLine());
 
         System.out.print("Edit product price: ");
-        int eidtProductPrice = arrayList.get(indexDeleteProct).setProductPrice(Integer.parseInt(sc.nextLine()));
-
+        product.setProductPrice(Integer.parseInt(sc.nextLine()));
     }
 
-    public static void main(String[] args) {
-        Product product1 = new Product("A123", "SamSung", 15000);
-        Product product2 = new Product("B123", "iPhone", 20000);
-        Product product3 = new Product("C123", "BlackBerry", 5000);
-        Product product4 = new Product("D123", "SonyEricson", 4000);
-
-        arrayList.add(product1);
-        arrayList.add(product2);
-        arrayList.add(product3);
-        arrayList.add(product4);
-
-        sort(true);
-        System.out.println();
-        sort(false);
-
-/*        arrayList.sort((o1, o2) -> o1.compareTo(o2));
-
-        for (Product product : arrayList) {
-            System.out.println("Ascending: " + product);
-        }
-
-        System.out.println();
-
-        arrayList.sort((o1, o2) -> o2.compareTo(o1));
-
-        for (Product product : arrayList) {
-            System.out.println("Descending: " + product);
-        }*/
-
-        System.out.println();
-        menu();
-
-    }
-
-    public static void menu() {
+    private static void searchProductMenu() {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Input the ID of product: ");
 
-        int choiceNumber;
-        do {
-            System.out.println("Choose from menu: [0.Exit] [1.Add] [2.Edit] [3.Delete] Product");
-            choiceNumber = Integer.parseInt(sc.nextLine());
+        String id = sc.nextLine();
+        Product product = searchProduct(id);
 
-            if (choiceNumber >= 0 && choiceNumber < 4) {
-                if (choiceNumber == 0) {
-                    System.out.println("See U again!^^!");
-                    break;
-                }
+        if (product.equals(null)) {
+            System.out.println("Searched product doesn't exists");
+        } else {
+            System.out.println("Searched product: "+product);
+        }
+    }
 
-                if (choiceNumber == 1) { //Add
-                    addProduct("W123","Oppo",5000);
-                    sort(true);
-                }
-
-                if (choiceNumber == 2) { //Edit
-                    editProduct("W123","Oppo",6000);
-                    sort(true);
-                }
-
-                    if (choiceNumber == 3) { //Delete
-                        System.out.println("Input index of product U wanna delete");
-                        int index = Integer.parseInt(sc.nextLine());
-
-                        arrayList.remove(index);
-
-                        for (Product product : arrayList) {
-                            System.out.println("List of product after deleted: " + product);
-                        }
-                    }
-                }
-        } while (choiceNumber > 0 && choiceNumber < 4) ;
+    public static Product searchProduct(String id) {
+        // Product (***)  whatever we return, have to store in a variable in the same type
+        for (int i = 0; i < arrayList.size(); i++) {
+            if (arrayList.get(i).getProductID().equals(id)) {
+                return arrayList.get(i);
+            }
+        }
+        return null;
     }
 }
 
