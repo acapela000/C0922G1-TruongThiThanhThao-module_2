0cddd4db467f3cebe290481e5d2aed6647ce5db7
acapela000 <mrtrados@gmail.com>
2022-11-28 01:12:20 +0700

    Edit&Finish.

--- src/ss11_Stack_Queue/CountTheOccurenceOfWordInArray.java
+++ src/ss11_Stack_Queue/CountTheOccurenceOfWordInArray.java
@@ -1,10 +1,31 @@
 package ss11_Stack_Queue;
 
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class CountTheOccurenceOfWordInArray {
     public static void main(String[] args) {
-        System.out.println("Input a character");
+        countWords();
+
+    }
+
+    private static void countWords() {
+        String sentence = "hi My name is my MY";
+        String[] sentenceSplit = sentence.split(" ");
+
+        TreeMap<String, Integer> treeMap = new TreeMap<>();
+
+        for (int i = 0; i < sentenceSplit.length; i++) {
+            String key = sentenceSplit[i].toUpperCase();
+            if (treeMap.containsKey(key)) {
+                int count = treeMap.get(key);
+                treeMap.put(key,count+1);
+            } else treeMap.put(key,1);
+        }
+        System.out.println(treeMap);
+    }
+
+    /*System.out.println("Input a character");
         Scanner character = new Scanner(System.in);
 
         String[] array = {"a","b","a","c"};
@@ -16,7 +37,6 @@ public class CountTheOccurenceOfWordInArray {
                 countOccurence++;
             }
             System.out.println(countOccurence);
-        }
-
-    }
+        }*/
+    
 }
